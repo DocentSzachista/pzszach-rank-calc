c09c845275d0f64aec499c2d2309140a3c3e9672
DocentSzachista <idontknow5325@gmail.com>
2023-01-23 23:53:43 +0100

    add description component

---
+++ src/components/Descritpion.js
@@ -0,0 +1,17 @@
+const Description = () =>{
+
+    return (
+        <div className="desc">
+            <h2>Kalkulator norm rankingowych PZszach</h2>
+            <div>
+                <h3>Co potrzebujesz</h3>
+                <p>
+                    Jedynie kategorie szachowe. Twoja i przeciwników. 
+                    Kalkulator nie uwzględnia tempa gry w jakim brałeś udział.
+                </p>
+            </div>
+        </div>
+    );
+    
+}
+export default Description;
